d876585123bf13935ecb7794ec29c4b7ec0d46cf
Joshua Buhler <josh@buhler.me>
2019-12-19 21:55:28 -0700

    Dummy routes for all / single runner

--- Sources/App/Routes/RunnerCollection.swift
+++ Sources/App/Routes/RunnerCollection.swift
@@ -9,9 +9,13 @@ import Vapor
 
 final class RunnerCollection: RouteCollection {
     func boot(router: Router) throws {
-        let runner = router.grouped("runner", Int.parameter)
+        let runner = router.grouped("runner")
         
-        runner.get () { req -> String in
+        runner.get ("all") { req -> String in
+            return "All runners"
+        }
+        
+        runner.get (Int.parameter) { req -> String in
             let bib = try req.parameters.next(Int.self)
             return "Runner: \(bib)"
         }
