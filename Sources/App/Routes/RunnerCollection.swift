f510f23d7200889b82e5914cca3849bda14c7e2a
Joshua Buhler <josh@buhler.me>
2019-12-19 21:59:49 -0700

    Notes

--- Sources/App/Routes/RunnerCollection.swift
+++ Sources/App/Routes/RunnerCollection.swift
@@ -7,6 +7,14 @@
 
 import Vapor
 
+/** Runner Ops:
+    - all runners
+    - single runner
+        - times
+        - check-in
+        - check-out
+        - dnf
+**/
 final class RunnerCollection: RouteCollection {
     func boot(router: Router) throws {
         let runner = router.grouped("runner")
@@ -18,6 +26,16 @@ final class RunnerCollection: RouteCollection {
         runner.get (Int.parameter) { req -> String in
             let bib = try req.parameters.next(Int.self)
             return "Runner: \(bib)"
+        }
+        
+        runner.post(Int.parameter, "in") { req -> String in
+            let bib = try req.parameters.next(Int.self)
+            return "Runner: \(bib) - IN"
+        }
+        
+        runner.post(Int.parameter, "out") { req -> String in
+            let bib = try req.parameters.next(Int.self)
+            return "Runner: \(bib) - OUT"
         }
     }
     
