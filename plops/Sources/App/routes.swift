4f26d87bfb8c734bd441881d42828de8381a4908
Joshua Buhler <josh@buhler.me>
2022-04-11 21:11:59 -0600

    Starting to fill out placeholder routes

--- plops/Sources/App/routes.swift
+++ plops/Sources/App/routes.swift
@@ -9,6 +9,24 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    let runners = app.grouped("runners")
+    
+    // TODO: should this just be "/runners/" ?
+    runners.get("all") { req -> String in
+        return "A list of all runners in the system"
+    }
+    
+    runners.get(":bib") { req -> String in
+        let bib = req.parameters.get("bib") ?? "NOT FOUND"
+        return "Info about runner with bib: \(bib)"
+    }
+    
+    runners.get(":bib", "lastlocation") { req -> String in
+        let bib = req.parameters.get("bib") ?? "NOT FOUND"
+        return "Info about a runner's last location with bib: \(bib)"
+    }
+    
 
     try app.register(collection: TodoController())
 }
