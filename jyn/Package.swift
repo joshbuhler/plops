754524d7c04478eb0b9e946801391505b9556dce
Joshua Buhler <josh@buhler.me>
2022-08-08 21:49:03 -0600

    Loading a file

--- jyn/Package.swift
+++ jyn/Package.swift
@@ -14,7 +14,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "jyn",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .copy("Resources/testLogs")
+                ]),
         .testTarget(
             name: "jynTests",
             dependencies: ["jyn"]),
--- jyn/Sources/jyn/main.swift
+++ jyn/Sources/jyn/main.swift
@@ -1,1 +1,14 @@
-print("Hello, world!")
+import Foundation
+
+print("May the Force be with us.")
+
+//let logURL = Bundle.main.url(forResource: "log_2019", withExtension: "log")
+//let logURL = URL(fileURLWithPath: "./testLogs/log_2019.log")
+let logURL = URL(fileURLWithPath: "/Users/josh/Projects/plops/jyn/Sources/jyn/Resources/log_2019.log")
+print ("logURL: \(logURL)")
+guard let fileMon = try? FileMonitor(url: logURL) else {
+    print ("Failed to create FileMonitor")
+    exit(-1)
+}
+
+print ("The Force was with us.")
